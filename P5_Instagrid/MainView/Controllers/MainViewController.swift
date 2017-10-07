0a7692a35a0cebbbcf4df5b73be5df8c1a04fc23
Richeux Antoine <richeux.antoin44@gmail.com>
2017-10-07 10:29:52 +0200

    Ajout de deux contrôleurs pour gérer le changement de configuration de la grille automatiquement

--- P5_Instagrid/MainView/Controllers/MainViewController.swift
+++ P5_Instagrid/MainView/Controllers/MainViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var photoContainer: PhotoContainerView!
+    @IBOutlet var dispositionSelected: [UIImageView]!
+    
+    var dispositionIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        dispositionIndex = 2
+        dispositionSelected[dispositionIndex].isHidden = false
+        photoContainer.createDisposition(disposition: dispositionIndex)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animate(alongsideTransition: { (UIViewControllerTransitionCoordinatorContext) -> Void in
+            self.photoContainer.resizeView(orientation: UIApplication.shared.statusBarOrientation, screenBounds: UIScreen.main.bounds)
+            
+        }, completion: { (UIViewControllerTransitionCoordinatorContext) -> Void in
+            self.photoContainer.createDisposition(disposition: self.dispositionIndex)
+            
+        })
+        super.viewWillTransition(to: size, with: coordinator)
     }
-
+    
+    // Choice a photo from iphone
+    @IBAction func selectPhoto(_ sender: Any) {
+        guard let button = sender as? UIButton else {
+            return
+        }
+    }
+    
+    // Choice the photo's disposition
+    @IBAction func selectDisposition(_ sender: Any) {
+        guard let button = sender as? UIButton else {
+            return
+        }
+        
+        for dispo in dispositionSelected {
+            dispo.isHidden = true
+        }
+        
+        dispositionIndex = button.tag
+        dispositionSelected[button.tag].isHidden = false
+        photoContainer.createDisposition(disposition: button.tag)
+        
+    }
 
 }
 
